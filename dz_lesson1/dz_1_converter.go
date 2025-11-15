a49263f00dbecfbdc55c8112584b806a7bff2e81
Azam Ghulomov <ghulomovazam7@gmail.com>
2025-11-15 16:29:40 +0500

    lesson1

--- dz_lesson1/dz_1_converter.go
+++ dz_lesson1/dz_1_converter.go
@@ -1,10 +1,11 @@
 package main
 
 func main() {
-
+//Hello Go!
+	println("Helo,Go!")	
+//Converter
 	amount := 100.0
 	sellRate := 1.2 
 	result := (amount * sellRate)
-	println(result)
-		
+	println(result)	
 }
--- homeworks/dz-1.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main1() {
-	println("Helo,Go!")
-}
